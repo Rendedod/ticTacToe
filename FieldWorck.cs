f7cc133558cf68d2aac3526ff2b75288c3d99dfb
Elias <eliaseromanov@gmail.com>
2024-07-03 22:42:11 +0700

    Добавьте файлы проекта.

---
+++ FieldWorck.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ticTacToe
+{
+    internal class PlayField
+    {
+        private int x = 3;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                if (value > 0)
+                    x = value;
+            }
+        }
+        private int y = 3;
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                if (value > 0)
+                    y = value;
+            }
+        }
+        public Figurs[,] Field
+        {
+            get
+            {
+                return createField();
+            }
+        }
+        public Figurs chekcViner(Figurs[,] field)
+        {
+            Figurs reference = Figurs.empty;
+            for (int i = 0; i < x && reference == Figurs.empty; i++)
+            {
+                reference = field[i, 0];
+                for (int j = 0; j < y; j++)
+                {
+                    if (field[i, 0] == Figurs.empty || field[i, 0] != field[i, j])
+                    {
+                        reference = Figurs.empty;
+                        break;
+                    }
+                }
+            }
+            for (int j = 0; j < y && reference == Figurs.empty; j++)
+            {
+                reference = field[0, j];
+                for (int i = 0; i < x; i++)
+                {
+                    if (field[0, j] == Figurs.empty || field[0, j] != field[i, j])
+                    {
+                        reference = Figurs.empty;
+                        break;
+                    }
+                }
+            }
+            if (x == y && reference == Figurs.empty)
+            {
+                int i = 0;
+                reference = field[0, 0];
+                while (i < x)
+                {
+                    if (reference != field[i, i] || reference == Figurs.empty)
+                    {
+                        i = 0;
+                        reference = Figurs.empty;
+                        break;
+                    }
+                    i++;
+                }
+                for (int j = y - 1; i < x; j--)
+                {
+                    reference = field[0, y - 1];
+                    if (reference != field[i, j] || reference == Figurs.empty)
+                    {
+                        reference = Figurs.empty;
+                        break;
+                    }
+                    i++;
+                }
+            }
+            return reference;
+        }
+
+        public Figurs[,] AddRandomPoztion(Figurs[,] field, Figurs item)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int i = random.Next(x);
+                int j = random.Next(y);
+                if (field[i, j] == Figurs.empty)
+                {
+                    field[i, j] = item;
+                    return field;
+                }
+            }
+        }
+        private Figurs[,] createField()
+        {
+            Figurs[,] field = new Figurs[3, 3];
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                    field[i, j] = Figurs.empty;
+
+            return field;
+        }
+
+    }
+    internal enum Figurs : short
+    {
+        empty,
+        cross,
+        zero,
+        draw,
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class Form1 : Form
+    {
+        private bool rightWay = true;
+        private bool bot = false;
+        bool winerGet = false;
+        private Image cross;
+        private Image zero;
+        private PictureBox[,] field = new PictureBox[3, 3];
+        private Figurs[,] figursField;
+        PlayField playField = new PlayField();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            init();
+        }
+
+        private void init()
+        {
+            Button rebuild = new Button {Parent = this, Size = new Size(100, 40), Top = 20, Left = 350, BackColor = Color.Black, Text = "Rebuild", ForeColor = Color.White, Cursor = Cursors.Hand };
+            rebuild.Click += rebuildAll;
+
+            Button bot = new Button { Parent = this, Size = new Size(100, 40), Top = 100, Left = 350, BackColor = Color.Black, Text = "Bot", ForeColor = Color.White, Cursor = Cursors.Hand };
+            bot.Click += StartBot;
+
+            WebClient wc = new WebClient();
+            cross = Image.FromStream(wc.OpenRead("https://cdn.onlinewebfonts.com/svg/download_26540.png"));
+            zero = Image.FromStream(wc.OpenRead("https://cdn.onlinewebfonts.com/svg/download_441992.png"));
+            figursField = playField.Field;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    field[i, j] = new PictureBox { Parent = this, Size = new Size(100, 100), Top = i * 100, Left = j * 100, BorderStyle = BorderStyle.FixedSingle, Tag = new Point(i, j), Cursor = Cursors.Hand, SizeMode = PictureBoxSizeMode.StretchImage };
+                    field[i, j].Click += figureClick;
+                }
+        }
+
+        private void StartBot(object sender, EventArgs e)
+        {
+            winerGet = false;
+            bot = !bot;
+            rebuildAll(null, null);
+        }
+        private void botStap()
+        {
+            Random random = new Random();
+            while (checkViner() == Figurs.empty && bot && coutEmpty() != 1)
+            {
+                int x = random.Next(3);
+                int y = random.Next(3);
+                if (field[x, y].Image == null )
+                {
+                    if (rightWay)
+                    {
+                        figursField[x, y] = Figurs.cross;
+                        field[x, y].Image = cross;
+                    }
+                    else
+                    {
+                        figursField[x, y] = Figurs.zero;
+                        field[x, y].Image = zero;
+                    }
+                    rightWay = !rightWay;
+                    break;
+                }
+            }
+            if (checkViner() != Figurs.empty || coutEmpty() == 1)
+            {
+                printWiner();
+            }
+
+        }
+
+        private void printWiner()
+        {
+            if (checkViner() != Figurs.empty)
+            {
+                MessageBox.Show($"Win : {checkViner()}");
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        field[i, j].Enabled = false;
+            }
+
+        }
+
+        private int coutEmpty()
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    if (figursField[i, j] == Figurs.empty)
+                    {
+                        count++;
+                    }
+                }
+            return count;
+        }
+
+        private void figureClick(object sender, EventArgs e)
+        {
+            PictureBox item = (PictureBox)sender;
+            if (item.Image == null && checkViner() == Figurs.empty)
+            {
+                int x = item.Location.X / 100;
+                int y = item.Location.Y / 100;
+                if (rightWay)
+                {
+                    figursField[x, y] = Figurs.cross;
+                    item.Image = cross;
+                }
+                else
+                {
+                    figursField[x, y] = Figurs.zero;
+                    item.Image = zero;
+                }
+                rightWay = !rightWay;
+                if (checkViner() == Figurs.empty)
+                {
+                    botStap();
+                }
+                else
+                {
+                    printWiner();
+                }
+            }
+        }
+
+        private Figurs checkViner()
+        {
+            return playField.chekcViner(figursField);
+
+        }
+
+        private void rebuildAll(object sender, EventArgs e)
+        {
+            winerGet = false;
+            Controls.Clear();
+            init();
+        }
+    }
+}
